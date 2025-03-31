86d08813a4b2476ad1b736732ac2cd7765646844
devpraveen002 <dev.praveen002@gmail.com>
2025-03-31 22:17:06 +0530

    either aws or Mock data

--- PDFOCRProcessor.API/Program.cs
+++ PDFOCRProcessor.API/Program.cs
@@ -52,6 +52,20 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //        builder.Configuration["AWS:SecretKey"],
 //        RegionEndpoint.GetBySystemName(builder.Configuration["AWS:Region"] ?? "us-east-1")));
 
+if (builder.Configuration.GetValue<bool>("UseAWS"))
+{
+    builder.Services.AddSingleton<AmazonTextractClient>(_ =>
+        new AmazonTextractClient(
+            builder.Configuration["AWS:AccessKey"],
+            builder.Configuration["AWS:SecretKey"],
+            RegionEndpoint.GetBySystemName(builder.Configuration["AWS:Region"] ?? "us-east-1")));
+    builder.Services.AddScoped<IOcrService, TextractOcrService>();
+}
+else
+{
+    builder.Services.AddScoped<IOcrService, MockOcrService>();
+}
+
 // Configure CORS
 builder.Services.AddCors(options =>
 {
@@ -65,7 +79,7 @@ builder.Services.AddCors(options =>
 
 // Register services
 //builder.Services.AddScoped<IOcrService, TextractOcrService>();
-builder.Services.AddScoped<IOcrService, MockOcrService>();
+//builder.Services.AddScoped<IOcrService, MockOcrService>();
 builder.Services.AddScoped<ITextProcessor, TextProcessor>();
 builder.Services.AddScoped<IDocumentFormatter, DocumentFormatter>();
 builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
